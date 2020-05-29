da79f7f2f7ec6b82bdf535afcee3d665180eeff7
amir kabiri <akabiridev@gmail.com>
2020-05-29 15:20:12 +0430

    MachineReadableRegexConverter improvement : prepend '?' to next item improved

--- js/machine-readable-regex-converter.js
+++ js/machine-readable-regex-converter.js
@@ -123,25 +123,9 @@ class MachineReadableRegexConverter{
      */
     convertLambda(regex, orSeparator = '+'){
         let hasLambda = false;
-        let prependToNext = null;
 
         for(let i = 0; i < regex.length; i++){
             let group = regex[i];
-
-            // if prependToNext is enable
-            if(prependToNext !== null){
-                // if current item is array, so insert prependToNext string after current item
-                if(Array.isArray(group)){
-                    // this code inserts an item after index of i
-                    regex.splice(i, 0, prependToNext);
-                }
-                // if current item is string, prepend prependToNext to the current item
-                else if(typeof group === 'string'){
-                    regex[i] = prependToNext + regex[i];
-                }
-
-                prependToNext = null;
-            }
 
             if(Array.isArray(group)){
                 // process subGroup recursively
@@ -149,9 +133,23 @@ class MachineReadableRegexConverter{
 
                 // if group was something like this : [['a+b'], '?']
                 // replace this group with ['a+b'] and put '?' in next index
+                // this action makes the result flatter and prettier
                 const lastItemOfSubGroup = subGroup[subGroup.length - 1];
                 if(subGroup.length === 2 && lastItemOfSubGroup === '?'){
-                    prependToNext = '?';
+                    // if next item is not exists, that means current is the last item
+                    if(regex[i + 1] === undefined){
+                        // so push '?' to the regex
+                        regex.push('?');
+                    }
+                    // if next item is an array, so insert '?' after current item
+                    else if(Array.isArray(regex[i + 1])){
+                        // this code inserts an item before index of i + 1
+                        regex.splice(i + 1, 0, '?');
+                    }
+                    // if next item is string, prepend '?' to the next item
+                    else if(typeof regex[i + 1] === 'string'){
+                        regex[i + 1] = '?' + regex[i + 1];
+                    }
                 }
             }else if(typeof group === 'string'){
                 group = group.split(orSeparator);
@@ -166,8 +164,6 @@ class MachineReadableRegexConverter{
                 }
             }
         }
-
-        if(prependToNext !== null) regex.push(prependToNext);
 
         if(hasLambda) return [regex, '?'];
 
